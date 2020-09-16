7f256f08aa75d8a734bad7792a8aaa0f332caa33
Jason Long <jason.long@salesforce.com>
2020-09-16 14:35:37 -0400

    Tons of UI work...want to get a quick check on multiple devices

--- app/app.js
+++ app/app.js
@@ -1,26 +1,167 @@
 'use strict';
 
-import algoliasearch from 'algoliasearch';
-import instantsearch from 'instantsearch.js';
-import { searchAPI } from './config.json';
-
-const searchClient = algoliasearch('L1292YARWK', searchAPI);
+const searchClient = algoliasearch('L1292YARWK', '71cf36ea528d375a21804d918d0c5ed4');
 
 const searchTalks = instantsearch({
   indexName: 'TEDTalks_talks',
   searchClient,
 });
 
-//
-
 searchTalks.addWidgets([
-  searchBox({
-    container: "#searchbox"
+  instantsearch.widgets.searchBox({
+    container: "#searchbox",
+    cssClasses:{
+      input: 'form-control'
+    },
+    placeholder: 'Search by title, description, speaker, event or topic...',
+    showReset: false,
+    showSubmit: false
   }),
 
-  hits({
-    container: "#hits"
-  })
+  instantsearch.widgets.configure({
+    hitsPerPage: 24,
+    enablePersonalization: true,
+  }),
+
+  instantsearch.widgets.clearRefinements({
+    container: '#clear-refinements',
+    cssClasses: {
+      button: 'btn btn-secondary btn-sm btn-block my-2',
+      disabled: 'btn btn-secondary btn-sm btn-block btn-disabled mb-2'
+    },
+    templates:{
+      resetLabel: 'Clear your selections'
+    },
+  }),
+
+  instantsearch.widgets.sortBy({
+    container: '#sort-by',
+    items: [
+      { label: 'Our Picks', value: 'TEDTalks_talks' },
+      { label: 'Most Viewed', value: 'talks_viewed_count_desc' },
+    ],
+    cssClasses:{
+      select: 'form-control'
+    }
+  }),
+
+  instantsearch.widgets.refinementList({
+    container: '#refinements-tags',
+    attribute: 'tags',
+    cssClasses: {
+      count: 'count badge badge-pill badge-light border'
+    }
+  }),
+
+  instantsearch.widgets.refinementList({
+    container: '#refinements-event_name',
+    attribute: 'event_name',
+    cssClasses: {
+      count: 'count badge badge-pill badge-light border'
+    }
+  }),
+
+  instantsearch.widgets.refinementList({
+    container: '#refinements-languages',
+    attribute: 'languages',
+    cssClasses: {
+      count: 'count badge badge-pill badge-light border'
+    }
+  }),
+
+  instantsearch.widgets.refinementList({
+    container: '#refinements-speakers',
+    attribute: 'speakers',
+    cssClasses: {
+      count: 'count badge badge-pill badge-light border'
+    }
+  }),
+
+  instantsearch.widgets.refinementList({
+    container: '#refinements-duration_range',
+    attribute: 'duration_range',
+    cssClasses: {
+      count: 'count badge badge-pill badge-light border'
+    }
+  }),
+
+  // Decided to do infiniteHits widget instead.
+/*   instantsearch.widgets.pagination({
+    container: '#pagination',
+    cssClasses: {
+      root: 'd-flex justify-content-center mb-5',
+      list: 'btn-group',
+      item: 'btn btn-sm btn-outline-primary px-3',
+      selectedItem: 'btn btn-sm btn-primary px-3',
+    },
+      showFirst: false,
+      showLast: false,
+      showNext: false,
+      showPrevious: false
+  }), */
+
+  // TODO: Add moment.js for relative date filtering
+
+  instantsearch.widgets.infiniteHits({
+    container: "#hits",
+    cssClasses: {
+      item: 'col-sm-4 col-md-3 col-lg-2 mb-4',
+      list: 'row',
+      loadMore: 'btn btn-lg btn-outline-primary',
+      disabledLoadMore: 'btn btn-lg btn-outline-primary btn-disabled'
+    },
+    templates: {
+      item: `
+
+        <div class="card h-100">
+          <div class="card-wrap">
+          <div class="card-img-top">
+            <a href="#" title="Link to {{name}}" data-toggle="modal" data-target="#modal-{{objectID}}">
+              <img src="{{image_url}}" alt="{{name}} Video Thumbnail" class="img-fluid" />
+            </a>
+          </div>
+          <div class="card-body">
+          <a href="#" title="Link to {{name}}" data-toggle="modal" data-target="#modal-{{objectID}}"><h4 class="hit-name">{{#helpers.highlight}}{"attribute" : "name"}{{/helpers.highlight}}</a>
+            </div>
+          </div>
+          </div>
+        </div>
+        <div class="modal fade" id="modal-{{objectID}}" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true" data-slug="{{slug}}">
+  <div class="modal-dialog modal-lg modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="ModalLabel">{{#helpers.highlight}}{"attribute" : "name"}{{/helpers.highlight}} <small class="text-muted">Presented by: {{#speakers}}
+        {{.}}
+      {{/speakers}}</small></h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <div class="row pb-3">
+        <div class="col-sm-4 align-self-center"><a href="https://www.ted.com/talks/{{slug}}"></a><img src="{{image_url}}" alt="{{name}} Video Thumbnail" class="img-fluid" /></a></div>
+        <div class="col-sm-8">
+      <p class="hit-description">{{#helpers.highlight}}{"attribute" : "description"}{{/helpers.highlight}}</p>
+      <small>Views: {{viewed_count}}</small>
+      <div class="tag-wrapper">
+      {{#tags}}
+        <span class="tag badge badge-pill badge-secondary">{{.}}</span>
+      {{/tags}}
+      </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-light btn-sm" data-dismiss="modal">Close</button>
+        <a href="https://www.ted.com/talks/{{slug}}" role="button" class="btn btn-primary btn-sm">Watch this Talk</a>
+      </div>
+    </div>
+  </div>
+</div>
+      `
+    }
+  }),
+
+
 ]);
 
-search.start();
+searchTalks.start();
--- app/loader.js
+++ app/loader.js
@@ -1,12 +1,14 @@
 'use strict';
 // Import Algolia
-import algoliasearch from 'algoliasearch';
+const algoliasearch = require('algoliasearch');
 // Algolia API Credentials -- Stored in config.json. See config-sample.json for format; obviously these are hidden for my safety!
-import { adminAPI } from './config.json';
+const { adminAPI } = require('./config.json');
 // Local files used for data
-import talks from '../data/tedtalks/talks.json';
-import speakers from '../data/tedtalks/speakers.json';
-import playlists from '../data/tedtalks/playlists.json';
+const talks = require('../data/tedtalks/talks.json');
+const speakers = require('../data/tedtalks/speakers.json');
+const playlists = require('../data/tedtalks/playlists.json');
+// Settings files for replica indicies
+const talks_viewed_count_desc_settings = require('../data/tedtalks/talks_viewed_count_desc_settings.json');
 
 // Initialize Algolia API
 const client = algoliasearch('L1292YARWK', adminAPI);
@@ -15,3 +17,6 @@ const client = algoliasearch('L1292YARWK', adminAPI);
 client.initIndex('TEDTalks_talks').saveObjects(talks);
 client.initIndex('TEDTalks_speakers').saveObjects(speakers);
 client.initIndex('TEDTalks_playlists').saveObjects(playlists);
+
+// Create replica indicies for the talks index and import their settings
+client.initIndex('talks_viewed_count_desc').setSettings(talks_viewed_count_desc_settings);
